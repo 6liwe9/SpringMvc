363b99cf5740789c4d8b4d53deea22a82400db41
liwe69 <lwenzhi@sino-cr.org>
2018-03-19 11:28:21 +0800

    commit

---
+++ src/main/java/test/liwe/contorller/A.java
@@ -0,0 +1,19 @@
+package test.liwe.contorller;
+
+public class A {
+	public String a;
+	public String b;
+	public String c;
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+}
